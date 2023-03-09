fc03b26d56ea0bbec41672e145e4dc9059b4ab14
David Waza <wazabanga00@gmail.com>
2023-03-09 08:53:36 +0100

    form validation

--- src/app/Contacts/contacts/page.js
+++ src/app/Contacts/contacts/page.js
@@ -1,7 +1,13 @@
-'use client'
+"use client";
 import { Container, Col, Row } from "react-bootstrap";
-import Contstyles from '../Contact.module.css'
+import Contstyles from "../Contact.module.css";
+import { useForm, ValidationError } from "@formspree/react";
+
 const Contact = () => {
+  const [state, handleSubmit] = useForm("mgebzekj");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
   return (
     <>
       <Container className={Contstyles.wrapper}>
@@ -11,43 +17,69 @@ const Contact = () => {
               <p>Contact us</p>
               <p>You can reach us via email or this form below.</p>
             </div>
-            <form>
+            <form onSubmit={handleSubmit}>
               <input
+                id="firstName"
                 type="text"
+                name="firstName"
                 placeholder="First Name"
                 required
                 maxLength={100}
                 className={Contstyles.input}
               />
               <input
+                id="lastName"
                 type="text"
+                name="lastName"
                 placeholder="Last Name"
                 required
                 maxLength={100}
                 className={Contstyles.input}
               />
               <input
-                type="Email"
+                id="email"
+                type="email"
+                name="email"
                 placeholder="Email"
                 required
                 maxLength={100}
                 className={Contstyles.input}
               />
+              <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+              />
               <input
+                id="number"
                 type="number"
+                name="number"
                 placeholder="Phone Number"
                 required
                 maxLength={100}
                 className={Contstyles.input}
               />
+              <ValidationError
+                prefix="Number"
+                field="number"
+                errors={state.errors}
+              />
               <textarea
-                name="MultiLine"
+                id="message"
+                name="message"
                 maxLength={3000}
                 placeholder="Leave us a message"
                 className={Contstyles.textArea}
               />
-              <div  className={Contstyles.buttonSubmit}>
-                <button>send enquiry</button>
+              <ValidationError
+                prefix="Message"
+                field="message"
+                errors={state.errors}
+              />
+              <div className={Contstyles.buttonSubmit}>
+                <button type="submit" disabled={state.submitting}>
+                  send enquiry
+                </button>
               </div>
             </form>
           </Col>
