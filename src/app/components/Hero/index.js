eaffb416c919cd1559813334b8546d0e93cec367
David Waza <wazabanga00@gmail.com>
2023-03-16 14:52:00 +0100

    fixed homepage mobile hero banner text

--- src/app/components/Hero/index.js
+++ src/app/components/Hero/index.js
@@ -8,7 +8,15 @@ const Hero = () => {
     <div>
       <Row className="g-0">
         <Col sm={6} xs={12}>
-          <div className={styles.contentWrapper}></div>
+          <div className={styles.contentWrapper}>
+            <div className={styles.mobileContent}>
+            <p>Transforming the future through innovation and excellence</p>
+              <p>
+                We are a multipurpose company that focus on long term
+                relationships with customers.
+              </p>
+            </div>
+          </div>
         </Col>
         <Col sm={6} className={`d-none d-md-block`}>
           <div className={styles.heroLeft}>
